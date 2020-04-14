5667c6606d80fe6e81f4f9786f3685d0145b6905
Alex Vassiliou <avassiliou@zendesk.com>
2020-04-14 17:42:40 +1000

    open and read html file

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 func main() {
+	fileName := flag.String("file", "ex1.html", "name of the html file to be parsed")
+	flag.Parse()
+
+	f, err := ioutil.ReadFile(*fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(f))
 
 }
