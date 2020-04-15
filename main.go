eb828a9e9e8974eb7a56758a4c63f8fd0d7332da
Alex Vassiliou <avassiliou@zendesk.com>
2020-04-15 11:39:18 +1000

    finding a href tags

--- main.go
+++ main.go
@@ -3,19 +3,43 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
+	"os"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
 	fileName := flag.String("file", "ex1.html", "name of the html file to be parsed")
 	flag.Parse()
 
-	f, err := ioutil.ReadFile(*fileName)
+	f, err := os.Open(*fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 
-	fmt.Println(string(f))
+	z := html.NewTokenizer(f)
 
+	for {
+		tt := z.Next()
+
+		switch {
+		case tt == html.ErrorToken:
+			return
+		case tt == html.StartTagToken:
+			t := z.Token()
+
+			isAnchor := t.Data == "a"
+			if isAnchor {
+				for _, a := range t.Attr {
+					if a.Key == "href" {
+						fmt.Println("Found href:", a.Val)
+						break
+					}
+				}
+			}
+
+		}
+	}
 }
